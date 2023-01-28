42e6315f88e94f1547e1a8fc6ed917c1bc9f18c9
yuukiiwai <yuuki.20020217@gmail.com>
2023-01-28 17:17:49 +0900

    update admin page

--- portfolio/index/admin.py
+++ portfolio/index/admin.py
@@ -1,8 +1,30 @@
 from django.contrib import admin
+from django import forms
 from .models import History,Strong,StrongExp,Studystate
 
 # Register your models here.
-admin.site.register(History)
+@admin.register(History)
+class HistoryAdmin(admin.ModelAdmin):
+    model = History
+    list_display = ["when","event","order"]
+
 admin.site.register(Strong)
-admin.site.register(StrongExp)
-admin.site.register(Studystate)
+
+class StrongExpForm(forms.ModelForm):
+    class Meta:
+        model = StrongExp
+        fields = ["exp"]
+
+    exp = forms.CharField(
+        widget=forms.Textarea( attrs={"maxlength":str(StrongExp.exp.field.max_length),} ) )
+
+@admin.register(StrongExp)
+class StrongExpAdmin(admin.ModelAdmin):
+    model = StrongExp
+    list_display = ["strong","exp"]
+    form = StrongExpForm
+
+@admin.register(Studystate)
+class StudystateAdmin(admin.ModelAdmin):
+    model = Studystate
+    list_display = ["state","tech","imgurl"]
